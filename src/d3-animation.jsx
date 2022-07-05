58305e10f1115bb0870883390d983a415c67e92c
ravikumar <ravikumarwolfd9130@gmail.com>
2022-07-05 17:35:40 +0530

    i

--- src/d3-animation.jsx
+++ src/d3-animation.jsx
@@ -10,66 +10,38 @@ export default function Chart({data}) {
       let height= 680;
       
 
-      const simulation = d3.forceSimulation()
-      .force("charge", d3.forceManyBody())
-      .force("link", d3.forceLink().id(d => d.id))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY())
-      .on("tick", ticked);
-
-  const svg = d3.create("svg")
-      .attr("viewBox", [-width / 2, -height / 2, width, height]);
-
-  let link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-    .selectAll("line");
-
-  let node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-    .selectAll("circle");
-
-  function ticked() {
-    node.attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-
-    link.attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);}
-        let drag = simulation => {
-  
-          function dragstarted(event, d) {
-            if (!event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-          }
-          
-          function dragged(event, d) {
-            d.fx = event.x;
-            d.fy = event.y;
-          }
-          
-          function dragended(event, d) {
-            if (!event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-          }
-          
-          return d3.drag()
-              .on("start", dragstarted)
-              .on("drag", dragged)
-              .on("end", dragended);
+      const chart = ()=> {
+        const simulation = d3.forceSimulation()
+            .force("charge", d3.forceManyBody())
+            .force("link", d3.forceLink().id(d => d.id))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY())
+            .on("tick", ticked);
+      
+        const svg = d3.create("svg")
+            .attr("viewBox", [-width / 2, -height / 2, width, height]);
+      
+        let link = svg.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+          .selectAll("line");
+      
+        let node = svg.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+          .selectAll("circle");
+      
+        function ticked() {
+          node.attr("cx", d => d.x)
+              .attr("cy", d => d.y);
+      
+          link.attr("x1", d => d.source.x)
+              .attr("y1", d => d.source.y)
+              .attr("x2", d => d.target.x)
+              .attr("y2", d => d.target.y);
         }
-
-
-       const update=() => {
-          const nodes = data.nodes.filter(d => contains(d, time));
-          const links = data.edges.filter(d => contains(d, time));
-          chart.update({nodes, links});
-        }
-
+      
+        
         return Object.assign(svg.node(), {
           update({nodes, links}) {
       
@@ -92,11 +64,45 @@ export default function Chart({data}) {
             simulation.nodes(nodes);
             simulation.force("link").links(links);
             simulation.alpha(1).restart().tick();
-            ticked(); // render now!
+            ticked(); 
           }
         });
+      }
+      chart()
 
+      const update=() => {
+        const nodes = data.nodes.filter(d => contains(d, time));
+        const links = data.edges.filter(d => contains(d, time));
+        chart.update({nodes, links});
+      }
+      update()
 
+    //  let contains = ({start, end}, time) => start <= time && time < end;
+
+    const drag = simulation => {
+  
+      function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+      
+      function dragged(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+      }
+      
+      function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }
+      
+      return d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended);
+    }
 
     },[data.lenght]
 
